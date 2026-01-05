e986c0385da82c6a13948d321f4889ab65eaa863
palvikascs242535-a11y <palvikascs242535@gmail.com>
2026-01-05 12:13:02 +0530

    Update S098 M2 P7.R

--- S098 M2 P7.R
+++ S098 M2 P7.R
@@ -1,13 +1,33 @@
-# One-Way ANOVA using Medical Insurance dataset
-
 # Load dataset
 data <- read.csv("insurance.csv")
 
-# Convert smoker column to factor
+# Convert smoker to factor
 data$smoker <- as.factor(data$smoker)
 
-# Apply One-Way ANOVA
-anova_result <- aov(charges ~ smoker, data = data)
+# Numeric variable
+y <- data$charges
+g <- data$smoker
 
-# Display ANOVA table
-summary(anova_result)
+# Overall mean
+gm <- mean(y)
+
+# Between Group Sum of Squares
+SSB <- sum(tapply(y, g, length) * (tapply(y, g, mean) - gm)^2)
+
+# Within Group Sum of Squares
+SSW <- sum(tapply(y, g, function(x) sum((x - mean(x))^2)))
+
+# Degrees of freedom
+dfb <- length(levels(g)) - 1
+dfw <- length(y) - length(levels(g))
+
+# F value
+F <- (SSB/dfb) / (SSW/dfw)
+
+# p-value
+p <- pf(F, dfb, dfw, lower.tail = FALSE)
+
+# Output
+F
+p
+
