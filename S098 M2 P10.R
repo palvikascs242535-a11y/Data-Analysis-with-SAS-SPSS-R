e6ae8635e263652bc4dba4bf8def804efb0e114f
palvikascs242535-a11y <palvikascs242535@gmail.com>
2026-01-12 13:00:28 +0530

    Update S098 M2 P10.R

--- S098 M2 P10.R
+++ S098 M2 P10.R
@@ -1,33 +1,9 @@
-# Load ggplot2 library
+# Practical 10 : Graphical Reports using ggplot2
+
+install.packages("ggplot2")
+
 library(ggplot2)
-
-# Load dataset
 data <- read.csv("SuperMarket Analysis.csv")
-
-
-# 1. Simple Bar Chart
-
-ggplot(data, aes(x = Product.line)) +
-  geom_bar(fill = "skyblue") +
-  labs(
-    title = "Bar Chart of Product Line",
-    x = "Product Line",
-    y = "Count"
-  )
-
-
-# 2. Simple Line Graph
-
-ggplot(data, aes(x = Date, y = Sales, group = 1)) +
-  geom_line() +
-  labs(
-    title = "Line Graph of Sales",
-    x = "Date",
-    y = "Sales"
-  )
-
-
-# 3. Simple Scatter Plot
 
 ggplot(data, aes(x = Sales, y = gross.income)) +
   geom_point() +
@@ -35,4 +11,26 @@ ggplot(data, aes(x = Sales, y = gross.income)) +
     title = "Scatter Plot of Sales vs Gross Income",
     x = "Sales",
     y = "Gross Income"
+  )
+
+pie_data <- as.data.frame(table(data$Product.line))
+colnames(pie_data) <- c("Product.line", "Count")
+
+
+ggplot(pie_data, aes(x = "", y = Count, fill = Product.line)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y") +
+  labs(title = "Pie Chart of Product Line Distribution")
+
+high_low <- data.frame(
+  Type = c("Low Sales", "High Sales"),
+  Value = c(min(data$Sales), max(data$Sales))
+)
+
+ggplot(high_low, aes(x = Type, y = Value, fill = Type)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "High-Low Sales Chart",
+    x = "Sales Type",
+    y = "Sales Value"
   )
