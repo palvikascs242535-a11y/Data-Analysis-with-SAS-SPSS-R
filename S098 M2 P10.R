ea9e93bc674fd4d6927d5d1d9c096d5cd239f6ad
palvikascs242535-a11y <palvikascs242535@gmail.com>
2026-01-17 18:41:13 +0530

    Update S098 M2 P10.R

--- S098 M2 P10.R
+++ S098 M2 P10.R
@@ -1,9 +1,9 @@
-# Practical 10 : Graphical Reports using ggplot2
-
-install.packages("ggplot2")
-
 library(ggplot2)
+
 data <- read.csv("SuperMarket Analysis.csv")
+
+# 1. Scatter Plot
+# Sales vs Gross Income
 
 ggplot(data, aes(x = Sales, y = gross.income)) +
   geom_point() +
@@ -13,20 +13,29 @@ ggplot(data, aes(x = Sales, y = gross.income)) +
     y = "Gross Income"
   )
 
+# 2. Pie Chart
+# Product Line Distribution
+
+# Prepare data for pie chart
 pie_data <- as.data.frame(table(data$Product.line))
 colnames(pie_data) <- c("Product.line", "Count")
 
-
+# Pie Chart
 ggplot(pie_data, aes(x = "", y = Count, fill = Product.line)) +
   geom_bar(stat = "identity", width = 1) +
   coord_polar("y") +
   labs(title = "Pie Chart of Product Line Distribution")
 
+# 3. High–Low Chart
+# Minimum and Maximum Sales
+
+# Create high-low data
 high_low <- data.frame(
   Type = c("Low Sales", "High Sales"),
   Value = c(min(data$Sales), max(data$Sales))
 )
 
+# High-Low Chart
 ggplot(high_low, aes(x = Type, y = Value, fill = Type)) +
   geom_bar(stat = "identity") +
   labs(
